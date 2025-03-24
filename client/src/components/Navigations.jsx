2b9b63121530fdb9d775af184244bb99822ff139
Denise Osoria <deniseosoria04@gmail.com>
2025-03-23 21:46:46 -0400

    updated css

--- client/src/components/Navigations.jsx
+++ client/src/components/Navigations.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { fetchUserAccount } from "../api";
+import "../Navigation.css";
 
 const Navigations = () => {
   const [user, setUser] = useState(null);
@@ -27,38 +28,43 @@ const Navigations = () => {
   };
 
   return (
-    <div className="nav">
-      {location.pathname !== "/" && (
-        <Link to="/">
-          <button className="nav-link">Home</button>
-        </Link>
-      )}
+    <nav className="nav-container">
+      <div className="nav-left">
+        {location.pathname !== "/" && (
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+        )}
+      </div>
 
-      {!user ? (
-        <Link to="/users/login">
-          <button className="nav-link">Login</button>
-        </Link>
-      ) : (
-        <>
-          <Link to="/create-event">
-            <button className="nav-link">Create Event</button>
+      <div className="nav-right">
+        {!user ? (
+          <Link to="/users/login" className="nav-button">
+            Login
           </Link>
-          <Link to="/booked-events">
-            <button className="nav-link">Booked Events</button>
-          </Link>
-          <Link to="/favorite-events">
-            <button className="nav-link">Favorites</button>
-          </Link>
-          <Link to="/users/account">
-            <button className="nav-link">Account</button>
-          </Link>
-          <button onClick={handleLogout} className="logout-button">
-            Logout
-          </button>
-        </>
-      )}
-    </div>
+        ) : (
+          <>
+            <Link to="/create-event" className="nav-button">
+              Create Event
+            </Link>
+            <Link to="/booked-events" className="nav-button">
+              Booked Events
+            </Link>
+            <Link to="/favorite-events" className="nav-button">
+              Favorites
+            </Link>
+            <Link to="/users/account" className="nav-button">
+              Account
+            </Link>
+            <button onClick={handleLogout} className="logout-button">
+              Logout
+            </button>
+          </>
+        )}
+      </div>
+    </nav>
   );
 };
 
 export default Navigations;
+
