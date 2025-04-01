8c54e95954206637ca8189f850aae52d4e96788e
Denise Osoria <deniseosoria04@gmail.com>
2025-03-31 22:04:25 -0400

    updated api url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 /** API Link */
-//export const API_URL = `${import.meta.env.VITE_API_URL}/api`;
-export const API_URL = `http://localhost:3001/api`;
+export const API_URL = `${import.meta.env.VITE_API_URL}/api`;
+// export const API_URL = `http://localhost:3001/api`;
 
 // ================================
 // Log in/Register fetch
