f70fea0f4b51bc6f5cf5587f9b999c8eedb587e4
Denise Osoria <deniseosoria04@gmail.com>
2025-04-24 12:58:57 -0400

    changed frontend url in server.js

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://eventbookie.netlify.app/"
+      "https://eventbookie.netlify.app"
     ],
     credentials: true,
   })
